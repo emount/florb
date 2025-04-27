0113e3bd6198b12d5058225d6ea7a3aafd7c6b05
Eldridge M. Mount IV <eldridge.mount@gmail.com>
2025-04-27 13:31:53 -0400

    Added stb_image.h back

--- florb.cpp
+++ florb.cpp
@@ -1,5 +1,5 @@
 // florb.cpp
-// Modern OpenGL 4.6 + C++17 full-screen textured rotating quad demo
+// Modern OpenGL 4.6 + C++17 full-screen semi-transparent 3D sphere
 
 #include <iostream>
 #include <vector>
@@ -17,7 +17,7 @@
 #define GL_GLEXT_PROTOTYPES
 #include <GL/glext.h>
 
-#include <cstring> // For memset
+#include <cstring>
 
 class FlorbApp {
 public:
@@ -26,56 +26,41 @@ public:
     void run();
 
 private:
-    // Window / GLX
     Display* display = nullptr;
     Window window = 0;
     GLXContext context = nullptr;
     int screen = 0;
     Atom wmDeleteMessage;
 
-    // Timing
-    std::chrono::steady_clock::time_point lastTime;
-
-    // OpenGL
     GLuint vao = 0;
     GLuint vbo = 0;
     GLuint ebo = 0;
     GLuint shaderProgram = 0;
-    GLuint texture = 0;
+    size_t indexCount = 0;
 
-    // Animation state
-    float angle = 0.0f;
-
-    // Methods
     void initWindow();
     void initGL();
     void createShaders();
-    void createGeometry();
-    void createTexture();
+    void createSphereGeometry();
     void mainLoop();
     void renderFrame();
-    void update();
     void handleEvents();
     void cleanup();
 
-    // Helpers
     GLuint compileShader(GLenum type, const char* source);
 };
 
 const char* vertexShaderSource = R"glsl(
 #version 460 core
 layout(location = 0) in vec3 aPos;
-layout(location = 1) in vec2 aTexCoord;
+out vec3 fragPos;
 
-uniform mat4 model;
 uniform mat4 projection;
-
-out vec2 TexCoord;
 
 void main()
 {
-    gl_Position = projection * model * vec4(aPos, 1.0);
-    TexCoord = aTexCoord;
+    fragPos = aPos;
+    gl_Position = projection * vec4(aPos, 1.0);
 }
 )glsl";
 
@@ -83,21 +68,18 @@ const char* fragmentShaderSource = R"glsl(
 #version 460 core
 out vec4 FragColor;
 
-in vec2 TexCoord;
-
-uniform sampler2D ourTexture;
+in vec3 fragPos;
 
 void main()
 {
-    FragColor = texture(ourTexture, TexCoord);
+    float intensity = 1.0 - length(fragPos);
+    intensity = clamp(intensity, 0.0, 1.0);
+    FragColor = vec4(vec3(1.0), 0.5 * intensity);
 }
 )glsl";
 
 FlorbApp::FlorbApp() {}
-
-FlorbApp::~FlorbApp() {
-    cleanup();
-}
+FlorbApp::~FlorbApp() { cleanup(); }
 
 GLuint FlorbApp::compileShader(GLenum type, const char* source) {
     GLuint shader = glCreateShader(type);
@@ -178,13 +160,8 @@ void FlorbApp::initWindow() {
 
     GLenum err = glewInit();
     if (err != GLEW_OK) {
-        std::cerr << "glewInit() returned error : (" << err << ")" << std::endl;
-	exit(-1);
-    }
-
-    if (!GLEW_VERSION_2_1) {
-        std::cerr << "glew version 2.1 is not available" << std::endl;
-	exit(-1);
+        std::cerr << "glewInit() returned error: (" << err << ")" << std::endl;
+        exit(-1);
     }
 
     XFree(vi);
@@ -194,6 +171,8 @@ void FlorbApp::initWindow() {
 void FlorbApp::initGL() {
     glViewport(0, 0, DisplayWidth(display, screen), DisplayHeight(display, screen));
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 void FlorbApp::createShaders() {
@@ -217,19 +196,48 @@ void FlorbApp::createShaders() {
     glDeleteShader(fragmentShader);
 }
 
-void FlorbApp::createGeometry() {
-    float vertices[] = {
-        // positions         // texture coords
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f,
-         0.5f, -0.5f, 0.0f,   1.0f, 0.0f,
-         0.5f,  0.5f, 0.0f,   1.0f, 1.0f,
-        -0.5f,  0.5f, 0.0f,   0.0f, 1.0f
-    };
+void FlorbApp::createSphereGeometry() {
+    const int sectorCount = 64;
+    const int stackCount = 64;
+    const float radius = 0.8f;
 
-    unsigned int indices[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
+    std::vector<float> vertices;
+    std::vector<unsigned int> indices;
+
+    for (int i = 0; i <= stackCount; ++i) {
+        float stackAngle = M_PI / 2 - i * M_PI / stackCount;
+        float xy = radius * cosf(stackAngle);
+        float z = radius * sinf(stackAngle);
+
+        for (int j = 0; j <= sectorCount; ++j) {
+            float sectorAngle = j * 2 * M_PI / sectorCount;
+            float x = xy * cosf(sectorAngle);
+            float y = xy * sinf(sectorAngle);
+            vertices.push_back(x);
+            vertices.push_back(y);
+            vertices.push_back(z);
+        }
+    }
+
+    for (int i = 0; i < stackCount; ++i) {
+        int k1 = i * (sectorCount + 1);
+        int k2 = k1 + sectorCount + 1;
+
+        for (int j = 0; j < sectorCount; ++j, ++k1, ++k2) {
+            if (i != 0) {
+                indices.push_back(k1);
+                indices.push_back(k2);
+                indices.push_back(k1 + 1);
+            }
+            if (i != (stackCount - 1)) {
+                indices.push_back(k1 + 1);
+                indices.push_back(k2);
+                indices.push_back(k2 + 1);
+            }
+        }
+    }
+
+    indexCount = indices.size();
 
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
@@ -238,45 +246,13 @@ void FlorbApp::createGeometry() {
     glBindVertexArray(vao);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    glBindVertexArray(0);
-}
-
-void FlorbApp::createTexture() {
-    const int texWidth = 64;
-    const int texHeight = 64;
-    std::vector<unsigned char> textureData(texWidth * texHeight * 3);
-
-    for (int y = 0; y < texHeight; ++y) {
-        for (int x = 0; x < texWidth; ++x) {
-            int checker = ((x / 8) % 2) ^ ((y / 8) % 2);
-            unsigned char color = checker ? 255 : 50;
-            textureData[(y * texWidth + x) * 3 + 0] = color;
-            textureData[(y * texWidth + x) * 3 + 1] = color;
-            textureData[(y * texWidth + x) * 3 + 2] = color;
-        }
-    }
-
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, textureData.data());
-    glGenerateMipmap(GL_TEXTURE_2D);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 }
 
 void FlorbApp::renderFrame() {
@@ -284,14 +260,6 @@ void FlorbApp::renderFrame() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glUseProgram(shaderProgram);
-
-    float rad = angle * 3.14159265f / 180.0f;
-    float model[16] = {
-        static_cast<float>(cos(rad)), static_cast<float>(-sin(rad)), 0.0f, 0.0f,
-        static_cast<float>(sin(rad)), static_cast<float>(cos(rad)),  0.0f, 0.0f,
-        0.0f,                         0.0f,                          1.0f, 0.0f,
-        0.0f,                         0.0f,                          0.0f, 1.0f
-    };
 
     int screenWidth = DisplayWidth(display, screen);
     int screenHeight = DisplayHeight(display, screen);
@@ -303,29 +271,13 @@ void FlorbApp::renderFrame() {
         0.0f, 0.0f,    0.0f, 1.0f
     };
 
-    GLuint modelLoc = glGetUniformLocation(shaderProgram, "model");
     GLuint projLoc = glGetUniformLocation(shaderProgram, "projection");
-
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, model);
     glUniformMatrix4fv(projLoc, 1, GL_FALSE, projection);
 
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    glUniform1i(glGetUniformLocation(shaderProgram, "ourTexture"), 0);
-
     glBindVertexArray(vao);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
 
     glXSwapBuffers(display, window);
-}
-
-void FlorbApp::update() {
-    auto now = std::chrono::steady_clock::now();
-    std::chrono::duration<float> deltaTime = now - lastTime;
-    lastTime = now;
-
-    angle += 45.0f * deltaTime.count();
 }
 
 void FlorbApp::handleEvents() {
@@ -338,8 +290,7 @@ void FlorbApp::handleEvents() {
                 cleanup();
                 exit(0);
             }
-        }
-        else if (event.type == KeyPress) {
+        } else if (event.type == KeyPress) {
             cleanup();
             exit(0);
         }
@@ -351,7 +302,6 @@ void FlorbApp::cleanup() {
     if (vao) glDeleteVertexArrays(1, &vao);
     if (vbo) glDeleteBuffers(1, &vbo);
     if (ebo) glDeleteBuffers(1, &ebo);
-    if (texture) glDeleteTextures(1, &texture);
 
     if (context) {
         glXMakeCurrent(display, None, nullptr);
@@ -371,16 +321,13 @@ void FlorbApp::run() {
     initWindow();
     initGL();
     createShaders();
-    createGeometry();
-    createTexture();
-    lastTime = std::chrono::steady_clock::now();
+    createSphereGeometry();
     mainLoop();
 }
 
 void FlorbApp::mainLoop() {
     while (true) {
         handleEvents();
-        update();
         renderFrame();
     }
 }
@@ -389,8 +336,7 @@ int main() {
     try {
         FlorbApp app;
         app.run();
-    }
-    catch (const std::exception& ex) {
+    } catch (const std::exception& ex) {
         std::cerr << "Fatal error: " << ex.what() << std::endl;
         return -1;
     }
