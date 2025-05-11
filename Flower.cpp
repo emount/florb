c7d500d50415ff95e7414d7fd7736760d1d2254a
Eldridge M. Mount IV <eldridge.mount@gmail.com>
2025-05-11 16:02:52 -0400

    Minor change to texture wrapping filters

--- Flower.cpp
+++ Flower.cpp
@@ -52,8 +52,8 @@ void Flower::loadImage() {
     glBindTexture(GL_TEXTURE_2D, textureID);
     FlorbUtils::glCheck("glBindTexture()");
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     
